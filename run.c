b43ccc947b19ac99607b26e12381cc8ad32cef38
JamesMcMurran <james@mcmurran.org>
2018-11-02 21:54:56 -0400

    Fixed formatting. Added Left and right phase logic

--- run.c
+++ run.c
@@ -6,7 +6,9 @@ char auth[] = "";
 char ssid[] = "";
 char pass[] = "";
 
-int input_pin1 = A0;
+
+int right_phase = A0;
+int left_phase = A1;
 
 BlynkTimer timer;
 
@@ -14,17 +16,17 @@ void loop()
 {
   timer.run(); // Initiates BlynkTimer
   Blynk.run();
-  
+
 }
 
 void setup() {
-  
+
   ArduinoOTA.onError([](ota_error_t error) { ESP.restart(); });
   ArduinoOTA.setHostname("PowerMonitor");
   ArduinoOTA.begin();
-  
-  pinMode(input_pin1, INPUT);
-  
+
+  pinMode(right_phase, INPUT);
+
   // Debug console
   Serial.begin(9600);
   Serial.print("Blynk Connecting");
@@ -32,7 +34,7 @@ void setup() {
   Serial.print("Blynk Connected");
 
   // Setup a function to be called every second
-  timer.setInterval(2000L, timeLoop);
+  timer.setInterval(2500L, timeLoop);
 }
 
 void timeLoop(){
@@ -44,28 +46,69 @@ BLYNK_CONNECTED() {
 }
 
 void updateValues(){
- int y=0;
- int c=0;
- int avgValue=0;
- int maxValue=0;
-  for (int i = 0; i < 10000; i++){
-    y = analogRead(input_pin1);
-    if(y > maxValue){
-      maxValue = y;
+  
+  int cr=0;
+  int cl=0;
+  int left_temp=0;
+  int right_temp=0;
+  int left_avg_value=0;
+  int right_avg_value=0;
+  int max_value_left=0;
+  int max_value_right=0;
+  
+  for (int i = 0; i < 5000; i++){
+
+    left_temp = analogRead(right_phase);
+    right_temp = analogRead(right_phase);
+
+    if(left_temp > max_value_left){
+      max_value_left = left_temp;
     }
-    if(y != 0){
-      avgValue +=y;
-      c++;
+
+    if(right_temp > max_value_right){
+      max_value_right = right_temp;
     }
-  }
-  if(avgValue!=0){
-    Blynk.virtualWrite(0, avgValue/c);
-    Serial.println(avgValue/c); 
-  }else{
-    Blynk.virtualWrite(0, 0);
-    Serial.println(0);
+
+    if(left_temp != 0){
+      left_avg_value += left_temp;
+      cl++;
+    }
+
+    if(right_temp != 0){
+      right_avg_value += right_temp;
+      cr++;
+    }
+
   }
   
-    Blynk.virtualWrite(22, maxValue);
-    Serial.println(maxValue);
+  //Serial.print("avg_value ");
+  if(left_avg_value!=0){
+    Blynk.virtualWrite(1, left_avg_value/cl);
+    //Serial.println(avg_value/cl);
+  }else{
+    Blynk.virtualWrite(1, 0);
+    //Serial.println(0);
+  }
+  
+  //Serial.print("avg_value ");
+  if(right_avg_value!=0){
+    Blynk.virtualWrite(0, right_avg_value/cr);
+    //Serial.println(avg_value/cr);
+  }else{
+    Blynk.virtualWrite(0, 0);
+    //Serial.println(0);
+  }
+
+  Blynk.virtualWrite(21, max_value_left);
+  Blynk.virtualWrite(22, max_value_right);
+
+  float div_max_left = max_value_left / 20.00;
+  float div_max_right = max_value_right / 20.00;
+  Blynk.virtualWrite(23, div_max_left);
+  Blynk.virtualWrite(24, div_max_right);
+  
+  //Serial.print("max value right ");
+  //Serial.println(max_value_right);
+  //Serial.print("max_value_right  / 20. ");
+  //Serial.println(divMax);
 }
